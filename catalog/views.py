f7f2fe9fdef6f97c35466f00d8b5806570cc94fc
IfeJayeola <ijjayeola@gmail.com>
2025-09-25 16:06:27 +0100

    feat: implemented pagination and filter

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import render
 from .models import Categories, User,  Product, Categories
 from .serializers import UserSerializers, ProductSerializers, CategoriesSerializers
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 # Create your views here.
 
@@ -12,6 +13,9 @@ class UserViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['category']
+    ordering_fields = ['price']
 
 class CategoriesViewSet(viewsets.ModelViewSet):
     queryset = Categories.objects.all()
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'catalog',
     'rest_framework_simplejwt',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -143,4 +144,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
